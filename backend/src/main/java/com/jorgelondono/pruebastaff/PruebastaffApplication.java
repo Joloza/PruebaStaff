01ee41783a2aa18f18fdaf7c8ade63ece171657a
Jorge Londoño <jmlz9427@gmail.com>
2023-02-13 16:04:37 -0500

    se estructura el proyecto

---
+++ backend/src/main/java/com/jorgelondono/pruebastaff/PruebastaffApplication.java
@@ -0,0 +1,13 @@
+package com.jorgelondono.pruebastaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebastaffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebastaffApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jorgelondono/pruebastaff/PruebastaffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgelondono.pruebastaff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebastaffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
