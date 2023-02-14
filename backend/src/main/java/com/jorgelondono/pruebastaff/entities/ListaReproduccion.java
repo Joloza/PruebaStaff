7062b527747849df5499f0f01344d4a7b39d035a
Jorge Londoño <jmlz9427@gmail.com>
2023-02-13 19:02:01 -0500

    se crean las funciones básicas

--- backend/src/main/java/com/jorgelondono/pruebastaff/entities/ListaReproduccion.java
+++ backend/src/main/java/com/jorgelondono/pruebastaff/entities/ListaReproduccion.java
@@ -1,29 +1,27 @@
 package com.jorgelondono.pruebastaff.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.util.List;
+
+import javax.persistence.*;
 
 import lombok.Data;
 
 @Data
 @Entity
-
 public class ListaReproduccion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(nullable = false, length = 6)
+    @Column(nullable = false)
     private String nombre;
 
-    @Column(nullable = true, length = 20)
+    @Column(nullable = true)
     private String descripcion;
 
-    @Column(nullable = true, length = 10)
-    private Object canciones;
+    @ElementCollection
+    @Column()
+    private List<String> cancion;
 
 }
